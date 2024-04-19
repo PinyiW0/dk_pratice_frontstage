f647fa3fb56102f11cc9e8e42ce77d7fd4402577
Pinyi <wpypy8@gmail.com>
2024-04-19 15:38:46 +0800

    fix

--- pages/text.vue
+++ pages/text.vue
@@ -11,8 +11,8 @@ const tabList2: any[] = [
 <template>
   <section class="w-full bg-white flex flex-col items-center justify-center border-1 border-solid border-#FFD79B">
     <div class="md:w-[calc(100%-160px)] xl:w-[calc(100%-320px)] p-5 flex flex-col items-start ">
-      <PageTitle label="我是text.vue"/>
-      <TabTest v-model="changeTab" :list="tabList2" class="flex gap-4 ml-11" />
+      <h1>我是text.vue </h1>
+      <TabText v-model="changeTab" :list="tabList2" class="flex gap-4 ml-11" />
     </div>
     <div class="md:w-[calc(100%-160px)] xl:w-[calc(100%-320px)] p-5 flex flex-col items-center justify-center">
       <NuxtPage />
